df812099f4a31fb93e697808f080a508ea25f7b0
doubtfulcoder <naveedtkabir@gmail.com>
2022-07-08 19:41:15 -0400

    Commented out object prettying code

--- chess.js
+++ chess.js
@@ -1841,20 +1841,20 @@ export const Chess = function (fen) {
       if (typeof move === 'string') {
         move_obj = move_from_san(move, sloppy)
       } else if (typeof move === 'object') {
-        var moves = generate_moves()
+        // var moves = generate_moves()
 
-        /* convert the pretty move object to an ugly move object */
-        for (var i = 0, len = moves.length; i < len; i++) {
-          if (
-            move.from === algebraic(moves[i].from) &&
-            move.to === algebraic(moves[i].to) &&
-            (!('promotion' in moves[i]) ||
-              move.promotion === moves[i].promotion)
-          ) {
-            move_obj = moves[i]
-            break
-          }
-        }
+        // /* convert the pretty move object to an ugly move object */
+        // for (var i = 0, len = moves.length; i < len; i++) {
+        //   if (
+        //     move.from === algebraic(moves[i].from) &&
+        //     move.to === algebraic(moves[i].to) &&
+        //     (!('promotion' in moves[i]) ||
+        //       move.promotion === moves[i].promotion)
+        //   ) {
+        //     move_obj = moves[i]
+        //     break
+        //   }
+        // }
       }
 
       /* failed to find move */
